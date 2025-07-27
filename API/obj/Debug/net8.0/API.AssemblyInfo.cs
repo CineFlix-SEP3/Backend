3f25bc0137bad7e72dd9c333027ac031a2043567
Bibek Paudel <bibekpaudel793@gmail.com>
2025-07-27 02:21:02 +0200

    Update .gitignore and assembly informational versions for API and GrpcClient

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61ca5480ec5fad531a5122a8439b88b9ad59e1bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c92f0feb790dfe9b873b8ebe63ec8b209762932")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
