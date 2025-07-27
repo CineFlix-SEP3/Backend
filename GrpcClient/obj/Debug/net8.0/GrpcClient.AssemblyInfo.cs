d900c49c023bfc0c6c464b4cfdf7629967a928d9
Bibek Paudel <bibekpaudel793@gmail.com>
2025-07-27 02:17:06 +0200

    Update assembly informational version for API and GrpcClient

--- GrpcClient/obj/Debug/net8.0/GrpcClient.AssemblyInfo.cs
+++ GrpcClient/obj/Debug/net8.0/GrpcClient.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GrpcClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82fe7c6468b3955a4f8edbcc7fd6a12d972e2a46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61ca5480ec5fad531a5122a8439b88b9ad59e1bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("GrpcClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GrpcClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
